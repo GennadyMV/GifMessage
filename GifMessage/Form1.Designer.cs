86e5f149ace653791054976baeaf14cfa17c06cc
Mikhail Gerasimov <Gerasimov@dvugms.khv.ru>
2017-06-02 15:13:54 +1000

    Init

---
+++ GifMessage/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace GifMessage
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.buttonWrite = new System.Windows.Forms.Button();
+            this.buttonRead = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxMsgIn = new System.Windows.Forms.TextBox();
+            this.textBoxMsgOut = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(12, 12);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoad.TabIndex = 0;
+            this.buttonLoad.Text = "Загрузить";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // buttonWrite
+            // 
+            this.buttonWrite.Location = new System.Drawing.Point(93, 12);
+            this.buttonWrite.Name = "buttonWrite";
+            this.buttonWrite.Size = new System.Drawing.Size(75, 23);
+            this.buttonWrite.TabIndex = 0;
+            this.buttonWrite.Text = "Записать";
+            this.buttonWrite.UseVisualStyleBackColor = true;
+            // 
+            // buttonRead
+            // 
+            this.buttonRead.Location = new System.Drawing.Point(174, 12);
+            this.buttonRead.Name = "buttonRead";
+            this.buttonRead.Size = new System.Drawing.Size(75, 23);
+            this.buttonRead.TabIndex = 0;
+            this.buttonRead.Text = "Прочитать";
+            this.buttonRead.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Встраиваемое сообщение";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(362, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Прочитанное сообщение";
+            // 
+            // textBoxMsgIn
+            // 
+            this.textBoxMsgIn.Location = new System.Drawing.Point(12, 79);
+            this.textBoxMsgIn.Multiline = true;
+            this.textBoxMsgIn.Name = "textBoxMsgIn";
+            this.textBoxMsgIn.Size = new System.Drawing.Size(249, 176);
+            this.textBoxMsgIn.TabIndex = 2;
+            // 
+            // textBoxMsgOut
+            // 
+            this.textBoxMsgOut.Location = new System.Drawing.Point(294, 79);
+            this.textBoxMsgOut.Multiline = true;
+            this.textBoxMsgOut.Name = "textBoxMsgOut";
+            this.textBoxMsgOut.Size = new System.Drawing.Size(249, 176);
+            this.textBoxMsgOut.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "GIF Изображение  (*.gif)|*.gif";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 3;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 291);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxMsgOut);
+            this.Controls.Add(this.textBoxMsgIn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonRead);
+            this.Controls.Add(this.buttonWrite);
+            this.Controls.Add(this.buttonLoad);
+            this.Name = "FormMain";
+            this.Text = "Стеганография";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Button buttonWrite;
+        private System.Windows.Forms.Button buttonRead;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxMsgIn;
+        private System.Windows.Forms.TextBox textBoxMsgOut;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ GifMessage/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GifMessage
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            if (this.openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.label3.Text = this.openFileDialog1.FileName;
+            }
+        }
+    }
+}
